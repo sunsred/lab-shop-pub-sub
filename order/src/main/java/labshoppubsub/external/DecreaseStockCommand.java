86f4fd642cba9cf0bb0f2725ce9c4d152a5ace2b
sunsred <rmrmtld@naver.com>
2023-03-29 13:09:47 +0900

    commit

---
+++ order/src/main/java/labshoppubsub/external/DecreaseStockCommand.java
@@ -0,0 +1,12 @@
+package labshoppubsub.external;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+
+@Data
+public class DecreaseStockCommand {
+
+    private Integer qty;
+}
